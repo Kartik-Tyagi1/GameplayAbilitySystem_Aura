0f57a5f37d0415d1f2918073955ce394d6983136
kt1661 <kt1661@nyu.edu>
2025-05-29 23:10:36 -0400

    Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright N^2 Entertainment
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright N^2 Entertainment
+
+#pragma once
+
+#include "CoreMinimal.h"
+
