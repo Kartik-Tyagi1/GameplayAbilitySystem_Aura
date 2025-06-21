559b7e80b687c447093839888536c69a597effd8
kt1661 <kt1661@nyu.edu>
2025-06-21 16:25:54 -0400

    Created gamemode and added camera to Aura

---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright N^2 Entertainment
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
